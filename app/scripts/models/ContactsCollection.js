b7d65c37ee5ad043b10fb78701241572d1e9569c
Aaron Emery <aemery14@gmail.com>
2015-11-19 13:50:24 -0500

    Update 2015-11-19T18:50:06.933Z

--- app/scripts/models/ContactsCollection.js
+++
@@ -1,8 +0,0 @@
-import Contact from 'models/Contact';
-
-var ContactsCollection = Backbone.Collection.extend({
-  model: Contact,
-  url: 'http://tiny-lasagna-server.herokuapp.com/collections/aaronsnewserver',
-});
-
-export default ContactsCollection;
